5eaf6dddc0cc710d945006b6622e53fa799c8e09
Alekhya1908 <61342303+Alekhya1908@users.noreply.github.com>
2020-06-21 16:20:14 +0530

    Add files via upload

---
+++ dy.js
@@ -0,0 +1,131 @@
+var root = document.getElementById("main");
+
+var div1 = document.createElement('div');
+div1.classList.add("card");
+root.appendChild(div1);
+
+var image = document.createElement('img');
+image.src='pic.png';
+image.height=100;
+image.width=100;
+image.alt="picture";
+div1.appendChild(image);
+
+var heading =document.createElement('h2');
+heading.textContent="Alekhya";
+div1.appendChild(heading);
+
+var heading =document.createElement('p');
+heading.textContent="Software developer";
+div1.appendChild(heading);
+
+div1.appendChild(document.createElement('hr'));
+
+var para = document.createElement('p');
+para.textContent="alekhyamand@gmail.com";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="9999988888";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="Rajahmundry";
+para.classList.add("p")
+div1.appendChild(para);
+
+var a1  = document.createElement("a");
+a1.href="inde.html";
+a1.textContent="view";
+a1.classList.add('a');
+div1.appendChild(a1);
+
+
+
+var div1 = document.createElement('div');
+div1.classList.add("card1");
+root.appendChild(div1);
+
+var image = document.createElement('img');
+image.src='pic.png';
+image.height=100;
+image.width=100;
+image.alt="picture";
+div1.appendChild(image);
+
+var heading =document.createElement('h2');
+heading.textContent="Vijaya";
+div1.appendChild(heading);
+
+var heading =document.createElement('p');
+heading.textContent="Student";
+div1.appendChild(heading);
+
+div1.appendChild(document.createElement('hr'));
+
+var para = document.createElement('p');
+para.textContent="vijaya45@gmail.com";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="6799988888";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="Rajahmundry";
+para.classList.add("p")
+div1.appendChild(para);
+
+var a1  = document.createElement("a");
+a1.href="inde1.html";
+a1.textContent="view";
+a1.classList.add('a');
+div1.appendChild(a1);
+
+
+var div1 = document.createElement('div');
+div1.classList.add("card2");
+root.appendChild(div1);
+
+var image = document.createElement('img');
+image.src='pic.png';
+image.height=100;
+image.width=100;
+image.alt="picture";
+div1.appendChild(image);
+
+var heading =document.createElement('h2');
+heading.textContent="Sruthi";
+div1.appendChild(heading);
+
+var heading =document.createElement('p');
+heading.textContent="Software developer";
+div1.appendChild(heading);
+
+div1.appendChild(document.createElement('hr'));
+
+var para = document.createElement('p');
+para.textContent="sruthi99@gmail.com";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="7789988888";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="Rajahmundry";
+para.classList.add("p")
+div1.appendChild(para);
+
+var a1  = document.createElement("a");
+a1.href="inde2.html";
+a1.textContent="view";
+a1.classList.add('a');
+div1.appendChild(a1);
+
